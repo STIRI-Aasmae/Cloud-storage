04bc03c9f944b5f086ac7ef23a161f645481bb5a
STIRI-Aasmae <stirias@gmail.com>
2020-05-19 00:14:55 +0100

    second commit

--- src/app/upload/form-upload/form-upload.component.ts
+++ src/app/upload/form-upload/form-upload.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
 import { FileUpload } from '../fileupload';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
 
 @Component({
   selector: 'form-upload',
@@ -8,6 +10,7 @@ import { FileUpload } from '../fileupload';
   styleUrls: ['./form-upload.component.scss']
 })
 export class FormUploadComponent implements OnInit {
+  metadatafile: any[];
   selectedFiles: FileList;
   currentFileUpload: FileUpload;
   progress: { percentage: number } = { percentage: 0 };
@@ -19,6 +22,8 @@ export class FormUploadComponent implements OnInit {
   selectFile(event) {
     this.selectedFiles = event.target.files;
   }
+
+  private basePath = '/uploads';
  
   upload() {
     const file = this.selectedFiles.item(0);
@@ -26,6 +31,19 @@ export class FormUploadComponent implements OnInit {
  
     this.currentFileUpload = new FileUpload(file);
     this.uploadService.pushFileToStorage(this.currentFileUpload, this.progress);
+    /*
+    const storageRef = firebase.storage().ref();
+    // Create a reference to the file whose metadata we want to retrieve
+     //this.metadatafile = storageRef.child(`${this.basePath}/${this.currentFileUpload.file.name}`);
+
+    // Get metadata properties
+    storageRef.child(`${this.basePath}/${this.currentFileUpload.file.name}`).getMetadata().then(function(metadata) {
+    // Metadata now contains the metadata for 'images/forest.jpg'
+    this.metadatafile=metadata; 
+    }).catch(function(error) {
+  // Uh-oh, an error occurred!
+    });
+     */
   }
 
 }
--- src/app/upload/list-upload/list-upload.component.ts
+++ src/app/upload/list-upload/list-upload.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import {FileUpload} from '../fileupload';
 import { UploadFileService } from '../upload-file.service';
-import { AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import * as firebase from 'firebase/app'
+import { __metadata } from 'tslib';
 
 @Component({
   selector: 'list-upload',
@@ -10,6 +11,7 @@ import { AngularFireDatabase, AngularFireList} from '@angular/fire/database';
   styleUrls: ['./list-upload.component.scss']
 })
 export class ListUploadComponent implements OnInit {
+  metadatafile;
   fileUploads: any[];
   //fileUploads: AngularFireList<FileUpload[]>;
   constructor(private uploadService: UploadFileService) { }
@@ -27,7 +29,26 @@ export class ListUploadComponent implements OnInit {
     ).subscribe(fileUploads => {
       this.fileUploads = fileUploads;
     });
+
+    
   }
+/*---*/
+private basePath = '/uploads';
+getmetadata(fileUpload: FileUpload){
+    const storageRef = firebase.storage().ref();
+    // Create a reference to the file whose metadata we want to retrieve
+     //this.metadatafile = storageRef.child(`${this.basePath}/${this.currentFileUpload.file.name}`);
+
+    // Get metadata properties
+    storageRef.child(`${this.basePath}/${fileUpload.file.name}`).getMetadata().then(function(metadata) {
+       this.metadatafile = metadata;
+    // Metadata now contains the metadata for 'images/forest.jpg'
+     
+    }).catch(function(error) {
+  // Uh-oh, an error occurred!
+    });
   
+  }
+/*---*/  
 
 }
